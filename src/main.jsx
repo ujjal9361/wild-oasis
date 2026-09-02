8d8f1a91b24f64d77376249901c6332fd713491a
Ujjal Shrestha <ujjalshrestha3141592@gmail.com>
2026-09-02 19:18:36 +0545

    Use react-error-boundary library to catch the errors when react is rendering instead of showing a white screen

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
+import GlobalStyles from "./styles/GlobalStyles.js";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onReset={() => window.location.replace("/")}
+    >
+      <App />
+    </ErrorBoundary>
   </StrictMode>,
 );
