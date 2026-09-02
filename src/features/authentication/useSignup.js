f1b9ac636118188be683961e9fcbf38189eb0405
Ujjal Shrestha <ujjalshrestha3141592@gmail.com>
2026-09-02 10:24:31 +0545

    Implement signup of users by already authenticated users, users need to verify their email

---
+++ src/features/authentication/useSignup.js
@@ -0,0 +1,15 @@
+import { useMutation } from "@tanstack/react-query";
+import { signup as signupApi } from "../../services/apiAuth";
+import toast from "react-hot-toast";
+
+export function useSignup() {
+  const { mutate: signup, isPending: isLoading } = useMutation({
+    mutationFn: signupApi,
+    onSuccess: (user) => {
+      toast.success(
+        "Account created successfully! Please verify the new account using the email address",
+      );
+    },
+  });
+  return { signup, isLoading };
+}
