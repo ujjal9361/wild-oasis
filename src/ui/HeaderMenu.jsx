f2c7cd79b2e8d002f9aa6259b3e57f2ee9ef1779
Ujjal Shrestha <ujjalshrestha3141592@gmail.com>
2026-09-02 11:44:49 +0545

    Update the header to include user avatar and a button that takes us to /account(to change user information)

---
+++ src/ui/HeaderMenu.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+import ButtonIcon from "./ButtonIcon";
+import { HiOutlineUser } from "react-icons/hi2";
+import { useNavigate } from "react-router-dom";
+import LogOut from "../features/authentication/LogOut";
+
+const StyledHeaderMenu = styled.ul`
+  display: flex;
+  gap: 0.5rem;
+`;
+function HeaderMenu() {
+  const navigate = useNavigate();
+  return (
+    <StyledHeaderMenu>
+      <li>
+        <ButtonIcon onClick={() => navigate("/account")}>
+          <HiOutlineUser />
+        </ButtonIcon>
+      </li>
+      <li>
+        <LogOut />
+      </li>
+    </StyledHeaderMenu>
+  );
+}
+
+export default HeaderMenu;
